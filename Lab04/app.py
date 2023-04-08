561c022b2d28141ab4c9c7a3d4193643e793cdb2
Sun <a24230928@gmail.com>
2023-04-08 15:02:29 +0800

    fix: lab04 element problem

--- Lab04/app.py
+++ Lab04/app.py
@@ -8,39 +8,53 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--window-size=1920,1080')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(service=ChromeService(
-    ChromeDriverManager().install()), options=options)
+options.add_argument("--headless")
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()), options=options
+)
 
-driver.get('https://www.nycu.edu.tw')
+driver.get("https://www.nycu.edu.tw")
 
 newsAEle = driver.find_element(
-    By.XPATH, '/html/body/div[1]/div/main/div/div/div/article/div/div/div/div/section[2]/div/div/div[1]/div/div/div/div/nav[1]/ul/li[2]/a')
+    By.XPATH,
+    "/html/body/div[1]/div/main/div/div/div/article/div/div/div/div/section[2]/div/div/div[1]/div/div/div/div/nav[1]/ul/li[2]/a",
+)
 newsAEle.click()
 
 firstArticleEle = driver.find_element(
-    By.XPATH, '/html/body/div[1]/div/main/div[1]/div/div/article/div/div/div/div/section/div/div/div/div/div/div[3]/div/div/div[2]/div[1]/ul/li[1]/a')
+    By.XPATH,
+    "/html/body/div[1]/div/main/div[1]/div/div/article/div/div/div/div/section/div/div/div/div/div/div[3]/div/div/div[2]/div[1]/ul/li[1]/a",
+)
 firstArticleEle.click()
 
 titleEle = driver.find_element(
-    By.XPATH, '/html/body/div[1]/div/main/div/div/div/article/header/h1')
+    By.XPATH, "/html/body/div[1]/div/main/div/div/div/article/header/h1"
+)
 contentEle = driver.find_element(
-    By.XPATH, '/html/body/div[1]/div/main/div/div/div/article/div')
+    By.XPATH, "/html/body/div[1]/div/main/div/div/div/article/div"
+)
 print(titleEle.text)
 print(contentEle.text)
 
-driver.switch_to.new_window('tab')
+driver.switch_to.new_window("tab")
 
-driver.get('https://www.google.com')
-inputEle = driver.find_element(
-    By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
-inputEle.send_keys('311551182')
+driver.get("https://www.google.com")
+inputEle = WebDriverWait(driver, 3).until(
+    EC.presence_of_element_located(
+        (
+            By.XPATH,
+            "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea",
+        )
+    )
+)
+inputEle.send_keys("311551182")
 inputEle.send_keys(Keys.ENTER)
 
-secondResultEle = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-    (By.XPATH, "(//h3[contains(@class, 'LC20lb')])[2]")))
+secondResultEle = WebDriverWait(driver, 3).until(
+    EC.presence_of_element_located((By.XPATH, "(//h3[contains(@class, 'LC20lb')])[2]"))
+)
 print(secondResultEle.text)
 
 driver.quit()
