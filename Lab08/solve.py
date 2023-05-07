d1be912b3461c6805cdc6675def5809ee1ba59e1
Sun <a24230928@gmail.com>
2023-05-07 15:41:53 +0800

    feat: add workflow for lab08

--- Lab08/solve.py
+++ Lab08/solve.py
@@ -2,26 +2,25 @@ import angr
 import sys
 
 main_addr = 0x4011A9
-find_addr = 0x401363
-avoid_addr = 0x40134D
-
-
-def twos_complement(hexstr, bits):
-    value = int(hexstr, 16)
-    if value & (1 << (bits - 1)):
-        value -= 1 << bits
-    return value
+find_addr = 0x40134F
+avoid_addr = 0x40133C
 
 
 class my_scanf(angr.SimProcedure):
     def run(self, format, buffer):
         simfd = self.state.posix.get_fd(sys.stdin.fileno())
-        data, ret_size = simfd.read_data(0x4)
+        data, ret_size = simfd.read_data(4)
         self.state.memory.store(buffer, data)
         return ret_size
 
 
 proj = angr.Project("./src/prog", load_options={"auto_load_libs": False})
+proj.hook_symbol(
+    "printf", angr.SIM_PROCEDURES["stubs"]["ReturnUnconstrained"](), replace=True
+)
+proj.hook_symbol(
+    "puts", angr.SIM_PROCEDURES["stubs"]["ReturnUnconstrained"](), replace=True
+)
 proj.hook_symbol("__isoc99_scanf", my_scanf(), replace=True)
 
 state = proj.factory.blank_state(addr=main_addr)
@@ -32,8 +31,10 @@ if simgr.found:
     ans = simgr.found[0].posix.dumps(sys.stdin.fileno())
     print(ans)
 
-    for i in range(0, len(ans), 0x4):
-        x = twos_complement((ans[i : i + 0x4][::-1]).hex(), 32)
-        print(x)
+    with open("solve_input", "w") as f:
+        for i in range(0, 15):
+            x = int.from_bytes(ans[i * 4 : i * 4 + 4], byteorder="little", signed=True)
+            f.write(f"{x}\n")
+            print(x)
 else:
     print("Failed")
